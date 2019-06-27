121bc65258fac10e6956692f72407075e705f1c6
TiffanyChou21 <TiffanyChou21@163.com>
2019-06-27 13:25:06 +0800

    zcf 1 commited

---
+++ CourseManagement2/backend/config/main-local.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => '9ipZeA6I2PEjvKNIOh-AMqZnPI3sVX7i',
+        ],
+    ],
+];
